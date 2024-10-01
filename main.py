0e994e5bef1ed17f2be6f50d009d923020cbdb94
Even Kallevik <even.kallevik@gmail.com>
2024-10-01 18:17:35 +0200

    Improvements

--- main.py
+++ main.py
@@ -5,21 +5,51 @@ def main():
 
     client = OpenAI()
 
-    # THIS IS THE INPUT TO CHATGPT. CHANGE THIS TO WHAT YOU NEED
-    content = "Say this is a test"
-
     # config
     model = "gpt-4o-mini"
-    role = "user"
+    role = "system"
 
-    response = client.chat.completions.with_raw_response.create(
+    object_codes = ["motor", "hjul", "vindu", "kjøler"]
+    object_codes_str = ', '.join(object_codes)
+
+    # todo: improve this
+    repair_descriptions = [
+        ['posisjon: hull i kjøler for vacumanl.', 'kort: Hull i kjøler for vacumsystem', 'beskrivelse: Hull i kjøler for vacumsystem\n'],
+        ["posisjon: motor", "kort: vindu", "beskrivelse: dekkluft\n"],
+    ]
+
+    repair_descriptions_str = ', '.join([item for sublist in repair_descriptions for item in sublist])
+
+    short_answers_only = True
+
+    content = f"""
+        Her en fullstendig oversikt over alle komponenter: [
+            ${object_codes_str}
+        ]
+    
+        En reparasjonsbestilling består av tre deler: posisjonstekst, korttekst og beskrivelse. Disse er manuelt registrert, og inneholder ofte motsetninger.
+        
+        Her er en liste over reperasjonsbestillinger: [
+            ${repair_descriptions_str}
+        ]
+        
+        For hver reparasjonsbestilling hvilken komponent tror du bestillingen handler om? 
+        
+        ${"Svar med en sannsynlighet for de mest relevante komponentene, adskilt av linjeskift" if short_answers_only else ""}
+    """
+
+    resp = client.chat.completions.create(
         model=model,
-        messages=[{"role": role, "content": content}],
+        messages=[
+            {"role": role, "content": content},
+        ],
+        temperature=0.1, # how creative chatgpt should be, from 0.0 to 1.0
+        max_tokens=300, # number of words-ish
+        frequency_penalty=0 # encourages repeated phrases
     )
 
-    print(f"Response Code: {response.status_code}")
-    print(f"Response Status: {response.status}")
-    print(response.content)
+    for choice in resp.choices:
+        print(choice.message.content)
 
 
 if __name__ == '__main__':
